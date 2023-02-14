e808ee5f0eae2e864f21dc5c83b66ceecba7fffb
Pierre-Jean MOREL <pj33720@gmail.com>
2023-02-14 00:13:11 -0500

    Tanieirb renaming

--- src/main/java/com/pjdevs/tanieirb/mixin/ServerMixin.java
+++ src/main/java/com/pjdevs/tanieirb/mixin/ServerMixin.java
@@ -1,16 +1,17 @@
-package net.fabricmc.example.mixin;
+package com.pjdevs.tanieirb.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.pjdevs.tanieirb.ServerMod;
+
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ServerMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ServerMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
